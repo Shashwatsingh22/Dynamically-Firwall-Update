f3e1cbf4eccfb0d5c5932d381076af633b940f33
Shashwat Singh (alias Ma4akaa1) <shashwatsingh71@gmail.com>
2022-09-27 18:17:22 +0530

    update

--- script.sh
+++ script.sh
@@ -1,2 +1,1 @@
-{ echo "--------->";python3 /home/ubuntu/pythonScriptForAutomation/main.py; date ;echo "triggered by Jenkins";echo "<-----------"; }  
-#>> /home/ubuntu/pythonScriptForAutomation/jenkins.log
+{ echo "--------->";python3 /home/ubuntu/pythonScriptForAutomation/main.py; date ;echo "triggered by Jenkins";echo "<-----------"; }  >> /home/ubuntu/pythonScriptForAutomation/jenkins.log
